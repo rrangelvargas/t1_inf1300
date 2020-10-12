efb962be505e730b1c602ce084f141610c0a883d
Thiago Nicácio <tgomesnicacio@gmail.com>
2020-10-12 19:40:15 -0300

    iniciando a tela de pedidos

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:t1_inf1300/forgotpassword.dart';
+import 'package:t1_inf1300/newrequestfragment.dart';
 
 class HomePage extends StatefulWidget {
   String title = 'Pharma +';
@@ -64,7 +64,7 @@ class _HomePageState extends State<HomePage> {
   _getDrawerItem(int pos) {
     switch (pos) {
       case 0:
-        return ForgotPassword();
+        return ;
       case 1:
         return;
       case 2:
