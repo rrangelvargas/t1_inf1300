83d73d34e3eab569f96ffa97224f16f156d5acd0
Thiago Nicácio <tgomesnicacio@gmail.com>
2020-10-12 22:33:44 -0300

    mudando item da lista de produtos

--- lib/newrequestfragment.dart
+++ lib/newrequestfragment.dart
@@ -29,15 +29,14 @@ class _NewRequestState extends State<NewRequest> {
               return GestureDetector(
                 onTap: () {},
                 child: ListTile(
-                  title: Text(product.name),
                   leading: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
-                      Text(product.description),
                       CircleAvatar(
                         backgroundColor: Colors.blueAccent,
                         backgroundImage: NetworkImage(product.image),
-                      )
+                      ),
+                      Text(product.description),
                     ],
                   ),
                 ),
@@ -48,7 +47,8 @@ class _NewRequestState extends State<NewRequest> {
   }
 
   void getProducts() async {
-    _products.add(Product(10, "Teste", "20", "Testeeeeeeee", "as"));
+    _products.add(new Product(10, "Teste", "20", "Testeeeeeeee", "as"));
+    _products.add(new Product(100, "Dip", "20", "Testeeeeeeee", "as"));
 
     setState(() {});
   }
