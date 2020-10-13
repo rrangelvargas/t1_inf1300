00a7c008443321ab4877d3401f7c774fbf309ec2
Thiago Nicácio <tgomesnicacio@gmail.com>
2020-10-13 01:07:28 -0300

    barra de busca, e mudando o item da lista de produtos

--- lib/newrequestfragment.dart
+++ lib/newrequestfragment.dart
@@ -20,36 +20,63 @@ class _NewRequestState extends State<NewRequest> {
   @override
   Widget build(BuildContext buildContext) {
     return Scaffold(
-      body: Center(
-        child: ListView.builder(
-            itemCount: _products.length,
-            itemBuilder: (BuildContext context, int index) {
-              Product product = _products[index];
-
-              return GestureDetector(
-                onTap: () {},
-                child: ListTile(
-                  leading: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.blueAccent,
-                        backgroundImage: NetworkImage(product.image),
-                      ),
-                      Text(product.description),
-                    ],
-                  ),
-                ),
-              );
-            }),
-      ),
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              onChanged: (value) {
+                //filterSearchResults(value);
+              },
+              //controller: editingController,
+              decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)))),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: _products.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Product product = _products[index];
+                  return GestureDetector(
+                      onTap: () {}, child: _buildRow(product));
+                }),
+          )
+        ],
+      )),
     );
   }
 
   void getProducts() async {
-    _products.add(new Product(10, "Teste", "20", "Testeeeeeeee", "as"));
-    _products.add(new Product(100, "Dip", "20", "Testeeeeeeee", "as"));
+    _products.add(new Product(10, "Teste", "20", "Testeeeeeeee",
+        "https://drogariasp.vteximg.com.br/arquivos/ids/168973-500-500/7896422507066.JPG.jpg?v=635651364067870000"));
+    _products.add(new Product(100, "Dip", "20", "Testeeeeeeee",
+        "https://drogariasp.vteximg.com.br/arquivos/ids/168973-500-500/7896422507066.JPG.jpg?v=635651364067870000"));
 
     setState(() {});
+  }
+
+  _buildRow(Product product) {
+    return ListTile(
+      leading: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image(
+            image: NetworkImage(product.image),
+            width: 50,
+            height: 50,
+          ),
+          Column(children: <Widget>[
+            Text(product.name),
+            Text(product.description)
+          ]),
+          Text(product.price),
+        ],
+      ),
+    );
   }
 }
