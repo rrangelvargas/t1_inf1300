6e6e944e81fc230d9f43b1c0102075dada9fe9ab
Thiago Nicácio <tgomesnicacio@gmail.com>
2020-10-15 03:52:05 -0300

    mudando a config do home

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:t1_inf1300/newrequestfragment.dart';
-import 'package:t1_inf1300/cart.dart';
 import 'package:t1_inf1300/order.dart';
 import 'package:t1_inf1300/MyOrders.dart';
 
 class HomePage extends StatefulWidget {
-  String title = 'Pharma +';
   HomePage();
 
   @override
@@ -13,19 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String title = 'Novo Pedido';
   int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(title: Text(title)),
       body: _getDrawerItem(_selectedIndex),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
@@ -37,13 +32,13 @@ class _HomePageState extends State<HomePage> {
             ListTile(
               title: Text('Novo Pedido'),
               onTap: () {
-                _onSelectItem(0);
+                _onSelectItem(0, 'Novo Pedido');
               },
             ),
             ListTile(
               title: Text('Meus Pedidos'),
               onTap: () {
-                _onSelectItem(1);
+                _onSelectItem(1, 'Meus Pedidos');
               },
             ),
             // ListTile(
@@ -55,7 +50,7 @@ class _HomePageState extends State<HomePage> {
             ListTile(
               title: Text('Sair'),
               onTap: () {
-                _onSelectItem(3);
+                _onSelectItem(3, '');
               },
             ),
           ],
@@ -69,16 +64,17 @@ class _HomePageState extends State<HomePage> {
       case 0:
         return new NewRequest();
       case 1:
-        return new Order();
+        return new MyOrders();
       case 2:
         return;
       case 3:
-        return MyOrders();
+        return;
     }
   }
 
-  _onSelectItem(int index) {
-    setState(() => _selectedIndex = index);
+  _onSelectItem(int index, String title) {
+    setState(() => {_selectedIndex = index});
+    setState(() => {this.title = title});
     Navigator.of(context).pop();
   }
 }
